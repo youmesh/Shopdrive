b42855e6707097d82f8af78bffbe8d9aeda34fcb
Umesh B Gowda <Umesh.B.Gowda@in.ril.com>
2016-11-24 12:47:03 +0530

    Done

---
+++ HTTPserver/Child.js
@@ -0,0 +1,25 @@
+var request = require('request');
+var inspect = require('util').inspect;
+var options = {
+    url: 'http://www.ajio.com',
+    path :'/rilfnlwebservices/',
+    headers: {
+        'Cache-Control' : 'no-cache'
+    }
+
+};
+ 
+
+   // www.ajio.com/rilfnlwebservices/v2/rilfnl/users/'umesh.b.gowda@ril.com'/carts/{cartId}/entries
+var i =0
+//while(true) {
+
+
+    request('https://www.ajio.com/cart/add', function (err, res, body) {
+
+        console.log(res.headers)
+
+   })
+  //  i++
+   // console.log(i)
+//}
